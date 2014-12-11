b419bf4fdd83ac4446074c5fa73b358bbe5ecc7a
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2014-12-10 17:47:00 -0700

    simplifiy redeploy script

--- vagrant-deploy.sh
+++ vagrant-deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-mvn verify
-[ $? -eq 0 ] && vagrant ssh -c "sudo cp -f /vagrant/target/*.war /var/lib/tomcat/webapps"
+mvn verify && vagrant ssh -c "sudo cp -f /vagrant/target/*.war /var/lib/tomcat/webapps"
