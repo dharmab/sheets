f0657fbb15facad04ef231973aa1f67febf3fbb8
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2014-12-21 10:56:13 -0700

    remove debug logging

--- src/main/java/com/dharmab/sheets/server/character/CharacterService.java
+++ src/main/java/com/dharmab/sheets/server/character/CharacterService.java
@@ -5,18 +5,14 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Singleton
 public class CharacterService {
     private DatabaseAccessor database;
-    private Logger logger;
 
     @Inject
-    public CharacterService(DatabaseAccessor database, Logger logger) {
+    public CharacterService(DatabaseAccessor database) {
         this.database = database;
-        this.logger = logger;
     }
 
     public Character get(int id) {
@@ -24,10 +20,7 @@ public class CharacterService {
     }
 
     public void persist(Character character) {
-        logger.log(Level.FINE, "before database.persist()");
         database.persist(character);
-        logger.log(Level.FINE, "after database.persist()");
-
     }
 
     public void delete(Character character) {
