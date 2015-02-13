a665be02cdf9076ba6c3d30dca57975e8b2e12e1
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2015-02-13 13:11:50 -0700

    resize fields dynamically

--- src/main/java/com/dharmab/sheets/client/widgets/IntegerSpinner.java
+++ src/main/java/com/dharmab/sheets/client/widgets/IntegerSpinner.java
@@ -3,6 +3,8 @@ package com.dharmab.sheets.client.widgets;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.editor.client.LeafValueEditor;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
@@ -33,7 +35,34 @@ public class IntegerSpinner extends Composite implements LeafValueEditor<Integer
 
     @UiHandler("field")
     void onValueChange(ValueChangeEvent<Integer> event) {
+        resize();
         fireEvent(event);
+    }
+
+    @UiHandler("field")
+    public void handleKeyPress(KeyPressEvent event) {
+        resize();
+    }
+
+    /**
+     * We have to handle key up events as well in order to handle backspace correctly.
+     *
+     * @param event
+     */
+    @UiHandler("field")
+    public void handleKeyUp(KeyUpEvent event) {
+        resize();
+    }
+
+    private void resize() {
+        String text = String.valueOf(getValue());
+
+        int numberOfDigits = text.equals("null") ? 2 : text.length();
+        if (numberOfDigits < 2) {
+            numberOfDigits = 2;
+        }
+        double width = 0.8 * numberOfDigits;
+        field.setWidth(width + "em");
     }
 
     @Override
@@ -49,6 +78,7 @@ public class IntegerSpinner extends Composite implements LeafValueEditor<Integer
     @Override
     public void setValue(Integer value, boolean fireEvents) {
         field.setValue(value, fireEvents);
+        resize();
     }
 
     @UiHandler("increment")
